c676dcf4dfc6d8c1493fb7d7e864285a6ad28d26
Alexey Vakhov <vakhov@gmail.com>
2026-03-14 11:46:45 +0300

    run.py

---
+++ run.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+import http.server
+import socketserver
+
+PORT = 8000
+
+with socketserver.TCPServer(("", PORT), http.server.SimpleHTTPRequestHandler) as httpd:
+    print(f"Serving at http://localhost:{PORT}")
+    httpd.serve_forever()
